c7be1dd7ef0e7ce2a94dd66700e21b4608109c43
SiroWirdo <sirowirdo@gmail.com>
2016-12-17 22:17:59 +0100

    First finished

--- src/KNN.java
+++ src/KNN.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -11,13 +12,14 @@ public class KNN {
 		this.trainData = trainData;
 	}
 	
-	
 	public ArrayList<String[]> testKNN(Data test){
 		ArrayList<String[]> results = new ArrayList<String[]>();
 		ArrayList<String[]> trainArguments = trainData.getArguments();
 		ArrayList<String[]> testArguments = test.getArguments();
+		ArrayList<String> testClasses = test.getClasses();
 
-		for(String[] testArg : testArguments){
+		for(int j = 0; j < testArguments.size(); j++){
+			String[] testArg = testArguments.get(j);
 	//		ArrayList<Integer> nearestNeigh = new ArrayList<Integer>();
 	//		ArrayList<Double> nearestNeighDist = new ArrayList<Double>();
 			TreeMap<Double, Integer> nearestNeigh = new TreeMap<Double, Integer>();
@@ -27,15 +29,36 @@ public class KNN {
 				double distance = euclDistance(trainArguments.get(i), testArg);
 				nearestNeigh.put(distance, i);
 				if(nearestNeigh.size() > k){
-					int temp = 1;
-					for (Map.Entry<Double, Integer> entry : nearestNeigh.entrySet()) {
-						if(temp > k){
-							nearestNeigh.remove(entry.getKey());
-						}
-					    temp++;
-					}
+					//int temp = 1;
+					//for(Map.Entry<Double, Integer> entry : nearestNeigh.entrySet()){
+					//	if(temp > k){
+					nearestNeigh.remove(nearestNeigh.lastKey());
+				//		}
+					//    temp++;
+				//	}
 				}
 			}
+			
+			HashMap<String, Integer> bestClasses = new HashMap<String, Integer>();
+			for(Map.Entry<Double, Integer> entry : nearestNeigh.entrySet()){
+				String classVal = trainData.getClasses().get(entry.getValue());
+				if(bestClasses.containsKey(classVal)){
+					bestClasses.put(classVal, bestClasses.get(classVal) + 1);
+				}else{
+					bestClasses.put(classVal, 1);
+				}
+			}
+			
+			String bestClass = "";
+			int bestVal = 0;
+			for(Map.Entry<String, Integer> entry : bestClasses.entrySet()){
+				if(entry.getValue() > bestVal){
+					bestClass = entry.getKey();
+					bestVal = entry.getValue();
+				}
+			}
+			String[] res = {testClasses.get(j),bestClass};
+			results.add(res);
 		}
 		
 		return results;
